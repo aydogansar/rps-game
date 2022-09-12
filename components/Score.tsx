34115b5f794131146c1296faa9fe8986b569f9a8
Aydoğan Sarı <aydogansari@hotmail.com>
2022-09-12 18:56:39 +0300

    Format code with prettier

--- components/Score.tsx
+++ components/Score.tsx
@@ -1,38 +1,34 @@
 import { styled } from "stitches.config";
 
 const Score = () => {
-    return (
-        <Wrapper>
-            <span>
-                SCORE
-            </span>
-            <span>
-                12
-            </span>
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <span>SCORE</span>
+      <span>12</span>
+    </Wrapper>
+  );
+};
 export default Score;
 
 const Wrapper = styled("div", {
-    flexCenter: "column",
+  flexCenter: "column", //stitches custom util
 
-    width: "170px",
-    height: "100%",
-    background: "$grayGradient",
-    borderRadius: "$md",
-    padding: "$10",
+  width: "170px",
+  height: "100%",
+  background: "$grayGradient",
+  borderRadius: "$md",
+  padding: "$10",
 
-    "& > span:first-of-type": {
-        color: "$scoreText",
-        marginBottom: "$5",
-        marginTop: "-$5",
-        letterSpacing: "2px",
-    },
+  "& > span:first-of-type": {
+    color: "$scoreText",
+    marginBottom: "$5",
+    marginTop: "-$5",
+    letterSpacing: "2px",
+  },
 
-    "& > span:last-of-type": {
-        fontSize: "64px",
-        color: "$darkText",
-        fontWeight: "$bold",
-    }
-})
+  "& > span:last-of-type": {
+    fontSize: "64px",
+    color: "$darkText",
+    fontWeight: "$bold",
+  },
+});
