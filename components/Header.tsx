4b27757f48bed11f837112ef7b6cea2c94df9170
Aydoğan Sarı <aydogansari@hotmail.com>
2022-09-17 10:14:03 +0300

    Update Score component with dynamic import

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,28 @@
+import dynamic from "next/dynamic";
 import { styled } from "stitches.config";
-import Score from "./Score";
+
+const Score = dynamic(() => import("./Score"), { ssr: false });
 
 const Header = () => {
-    return (
-        <Wrapper>
-            <img src="./logo.svg" />
-            <Score />
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <img src="./logo.svg" />
+      <Score />
+    </Wrapper>
+  );
+};
 export default Header;
 
 const Wrapper = styled("div", {
-    position: "relative",
-    top: "$12",
+  position: "relative",
+  top: "$12",
 
-    flexBetween: "center",
+  flexBetween: "center",
 
-    width: "100%",
-    height: "155px",
+  width: "100%",
+  height: "155px",
 
-    padding: "$4",
-    border: "2px solid $headerOutline",
-    borderRadius: "$2xl"
+  padding: "$4",
+  border: "2px solid $headerOutline",
+  borderRadius: "$2xl",
 });
