b9685491b262c404b777f3d0bf225d955d929d01
Aydoğan Sarı <aydogansari@hotmail.com>
2022-09-18 16:12:10 +0300

    Update favicon and title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,29 +2,31 @@ import { Html, Head, Main, NextScript } from "next/document";
 import { getCssText } from "../stitches.config";
 
 export default function Document() {
-    return (
-        <Html>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="true"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap"
-                    rel="stylesheet"
-                />
+  return (
+    <Html>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="icon" type="image/x-icon" href="./favicon.png" />
+        <title>RPS+ Game</title>
 
-                <style
-                    id="stitches"
-                    dangerouslySetInnerHTML={{ __html: getCssText() }}
-                />
-            </Head>
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
